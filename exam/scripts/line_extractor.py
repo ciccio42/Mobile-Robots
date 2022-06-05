e656af1d2aaf47c6edb30237feffad28d2c58d76
ciccio42 <francescorosa97@virgilio.it>
2022-06-05 10:10:54 +0200

    [ADD] Line extractor based on Canny

---
+++ exam/scripts/line_extractor.py
@@ -0,0 +1,59 @@
+import sys
+import math
+import cv2 as cv
+import numpy as np
+
+def main(argv):
+    
+    default_file = '/home/ciccio/Desktop/catkin_ws/src/exam/maps/map6.pgm'
+    
+    # Loads an image
+    src = cv.imread(default_file, cv.IMREAD_GRAYSCALE)
+    # Check if image is loaded fine
+    if src is None:
+        print ('Error opening image!')
+        print ('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
+        return -1
+    
+    src_gaussian = cv.GaussianBlur(src, (5, 5), 2)
+    dst = cv.Canny(src_gaussian, 30, 60)
+    
+    # apply dilatation
+    #kernel = np.ones((5, 5), np.uint8)
+    #dst = cv.dilate(dst, kernel, iterations=1)
+    #cv.imshow("dilatation", dst)
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+    
+    lines = cv.HoughLines(dst, 1, np.pi / 180, 50, None, 0, 0)
+    
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+    
+    
+    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 2, 10)
+    print(f"Number of lines {len(linesP)}")
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
+    
+    cv.imshow("Source", src_gaussian)
+    cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+    
+    cv.waitKey()
+    return 0
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
