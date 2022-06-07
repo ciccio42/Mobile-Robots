9156828ea120807590148989afb515a5c887af24
ciccio42 <francescorosa97@virgilio.it>
2022-06-07 16:35:51 +0200

    [ADD] New paths and baseline for feature extractor

--- exam/scripts/line_extractor.py
+++ exam/scripts/line_extractor.py
@@ -2,52 +2,79 @@ import sys
 import math
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
-def main(argv):
-    
-    default_file = '/home/ciccio/Desktop/catkin_ws/src/exam/maps/map6.pgm'
+MAP_RESOLUTION = 0.05
+
+def main(default_file = None):
     
     # Loads an image
     src = cv.imread(default_file, cv.IMREAD_GRAYSCALE)
+    
+    src_numpy = np.asarray(src)
+    cv.imshow("Original map image", src)
+    cv.waitKey(0)
+    print(f"Image size {src_numpy.shape}")
+
+    """ 
+    # convert pixel into continues value
+    obstacle_distance_list = []
+
+    for h in range(src_numpy.shape[0]):
+        for w in range(src_numpy.shape[1]):
+            if src_numpy[h][w] == 0:
+                obstacle_distance_list.append([round(w*MAP_RESOLUTION, 2), round(h*MAP_RESOLUTION, 2), 1])
+                print(f"Original pixel [h][w] {h}-{w} - Continue value [x][y] {obstacle_distance_list[-1][0]}-{obstacle_distance_list[-1][1]}") 
+    
+
+    # rotate to plot correctly
+    x_plot_frame_np = np.zeros(len(obstacle_distance_list))
+    y_plot_frame_np = np.zeros(len(obstacle_distance_list))
+
+    A_plot_frame = np.zeros((3,3))
+    A_plot_frame[0][0] = 1
+    A_plot_frame[1][1] = -1
+    A_plot_frame[2][2] = 1
+    A_plot_frame[1][2] = src_numpy.shape[0] * MAP_RESOLUTION
+    print(f"Homogeneous plot_frame to image_frame\n{A_plot_frame}")
+    for indx, point in enumerate(obstacle_distance_list):
+        point_image_frame = np.array([obstacle_distance_list[indx]]).transpose()
+        point_plot_frame = np.matmul(A_plot_frame, point_image_frame)
+        print(point_plot_frame)
+        x_plot_frame_np[indx] = point_plot_frame[0][0]
+        y_plot_frame_np[indx] = point_plot_frame[1][0]
+        
+    
+    plt.scatter(x_plot_frame_np, y_plot_frame_np, marker="o", s=0.01, color="black")
+    plt.show()
+    """
+    
     # Check if image is loaded fine
     if src is None:
         print ('Error opening image!')
         print ('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
         return -1
     
-    src_gaussian = cv.GaussianBlur(src, (5, 5), 2)
-    dst = cv.Canny(src_gaussian, 30, 60)
-    
-    # apply dilatation
-    #kernel = np.ones((5, 5), np.uint8)
-    #dst = cv.dilate(dst, kernel, iterations=1)
-    #cv.imshow("dilatation", dst)
+    src_gaussian = cv.GaussianBlur(src, (5, 5), 1)
+    dst = cv.Canny(src_gaussian, threshold1=100, threshold2=200, apertureSize=3, L2gradient=True)
     # Copy edges to the images that will display the results in BGR
     cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
-    
-    lines = cv.HoughLines(dst, 1, np.pi / 180, 50, None, 0, 0)
-    
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
-    
-    
-    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 2, 10)
+        
+    linesP = cv.HoughLinesP(image=dst,rho=0.5, theta=np.pi/360, threshold=10, lines=np.array([]), minLineLength=10, maxLineGap=10)
     print(f"Number of lines {len(linesP)}")
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
-            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
+            print(f"{i}-th line {l}")
+            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA)
     
+    # convert points from pixel to continuos space
+    # each row  
+    lines_continous = np.zeros((len(linesP), 4))
+    for i in range(0, len(linesP)):
+        pass
+
     cv.imshow("Source", src_gaussian)
     cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
     cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
@@ -56,4 +83,5 @@ def main(argv):
     return 0
     
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    default_file = '/home/ciccio/Desktop/catkin_ws/src/exam/maps/map6.pgm'
+    main(default_file=default_file)
